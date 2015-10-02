f47f0d191d1d785de26dbe9d1efc86f27c20b0a7
Jorge Blasco <jorge.blasco.1@city.ac.uk>
2015-10-02 17:00:17 +0100

    File renaming

--- acid/communication_channel_prolog_generator.py
+++ acid/communication_channel_prolog_generator.py
@@ -13,32 +13,32 @@ recv_results = []
 for subfolder in folders:
     app_folder = folder+"/"+subfolder
     print app_folder
-    trans_intent_result = check_output(["./acid/run_trans_intent_detector.py", "1",app_folder])
+    trans_intent_result = check_output(["./acid/trans_intent_prolog_generator.py", "1",app_folder])
     trans_results.append(trans_intent_result)
     with open(output_name+"_trans_intent_result_"+subfolder, "w") as text_file:
         text_file.write(trans_intent_result)
     print "Trans_intent for "+app_folder+" finished"
-    recv_intent_result = check_output(["./acid/run_recv_intent_detector.py", "1",app_folder])
+    recv_intent_result = check_output(["./acid/recv_intent_prolog_generator.py", "1",app_folder])
     recv_results.append(recv_intent_result)
     with open(output_name+"_recv_intent_result_"+subfolder, "w") as text_file:
         text_file.write(recv_intent_result)
     print "Recv_intent for "+app_folder+" finished"
-    trans_external_storage_result = check_output(["./acid/run_trans_external_storage_detector.py", "1",app_folder])
+    trans_external_storage_result = check_output(["./acid/trans_external_storage_prolog_generator.py", "1",app_folder])
     trans_results.append(trans_external_storage_result)
     with open(output_name+"_trans_external_storage_result_"+subfolder, "w") as text_file:
         text_file.write(trans_external_storage_result)
     print "Trans_external storage for "+app_folder+" finished"
-    recv_external_storage_result = check_output(["./acid/run_recv_external_storage_detector.py", "1",app_folder])
+    recv_external_storage_result = check_output(["./acid/recv_external_storage_prolog_generator.py", "1",app_folder])
     recv_results.append(recv_external_storage_result)
     with open(output_name+"_recv_external_storage_result_"+subfolder, "w") as text_file:
         text_file.write(recv_external_storage_result)
     print "Recv_external storage for "+app_folder+" finished"
-    trans_sharedprefs_result = check_output(["./acid/run_trans_sharedprefs_detector.py", "1",app_folder])
+    trans_sharedprefs_result = check_output(["./acid/trans_sharedprefs_detector_prolog_generator.py", "1",app_folder])
     trans_results.append(trans_sharedprefs_result)
     with open(output_name+"_trans_sharedprefs_result_"+subfolder, "w") as text_file:
         text_file.write(trans_sharedprefs_result)
     print "Trans_sharedprefs for "+app_folder+" finished"
-    recv_sharedprefs_result = check_output(["./acid/run_recv_sharedprefs_detector.py", "1",app_folder])
+    recv_sharedprefs_result = check_output(["./acid/recv_sharedprefs_prolog_generator.py", "1",app_folder])
     recv_results.append(recv_sharedprefs_result)
     with open(output_name+"_recv_sharedprefs_result_"+subfolder, "w") as text_file:
         text_file.write(recv_sharedprefs_result)
