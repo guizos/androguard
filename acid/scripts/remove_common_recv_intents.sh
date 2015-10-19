6bf96907a8c278b05ae45d4193630f1013bbdda9
Jorge Blasco <jorge.blasco.1@city.ac.uk>
2015-10-19 11:29:39 +0100

    script changed

--- acid/scripts/remove_common_recv_intents.sh
+++ acid/scripts/remove_common_recv_intents.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-grep -v -f scripts/common_recv_patterns $1
+grep -v -f acid/results/intent_data/common_intent_actions $1 | grep -v -f acid/results/intent_data/google_intent_actions 
